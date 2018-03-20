b37c493bc246f9d249beffc6677dcaa5fdf447fd
C272 <realrussianmanconfirmed@gmail.com>
2018-03-20 13:40:48 +0000

    Finished RAM storage, emulating interpreter.
    Memory storage is handled using white space as separation, address on the left and opcode on the right. A compiler is soon to come under the same project.

--- lmc.js
+++ lmc.js
@@ -2,8 +2,9 @@
 var fs = require('fs');
 
 //Creating all memory slots.
+var mem = [];
 for (var i=0; i<100; i++) {
-	eval("var m"+i+" = 1");
+	mem.push(000);
 }
 
 //Creating ACC, PC, IR, and AR.
@@ -25,12 +26,12 @@ if (process.argv.length<3) {
 	if (fileext!=".lmc") {
 		console.log("LMC E01: Invalid file extension '"+fileext+"'.");
 	} else {
-		//EXT Valid, sending to interpreter.
-		interpreter(filename);
+		//EXT Valid, sending to memory loader.
+		memload(filename);
 	}
 }
 
-function interpreter(filename) {
+function memload(filename) {
 	//Attempting to read file.
 	try {
 		fs.readFile(filename, 'utf8', function(err, data) {
@@ -45,11 +46,93 @@ function interpreter(filename) {
   				data[i] = data[i].split(" ");
   			}
 
-  			//Unfinished.
+  			//Saving instructions in file to addresses.
+  			for (i in data) {
+  				//Removing header.
+  				if (i!=0) {
+	  				//Verifying they're valid addresses.
+	  				try {
+	  					dint = parseInt(data[i][0]);
+	  				}
+	  				catch (err) {
+	  					console.log("LMC EO3: Invalid address at line "+(i+1)+".");
+	  					process.exit(1);
+	  				}
+
+	  				//Checking address is within range.#
+	  				if (dint>99 || dint<0) {
+	  					console.log("LMC EO4: Address out of range at line "+(i+1)+".");
+	  					process.exit(1);
+	  				} else {
+	  					//Valid, now checking Opcodes.
+	  					var opcode = data[i][1];
+	  					var validOp = false;
+	  					console.log(opcode.substring(0,1));
+
+	  					//Checking opcode substrings and operators.
+	  					if (opcode==901 || opcode==902) {
+	  						validOp = true;
+	  					} else if (opcode.length==3 && opcode.substring(0, 1)==5) {
+	  						validOp = true;
+	  					} else if (opcode.length==3 && opcode.substring(0, 1)==3) {
+	  						validOp = true;
+	  					} else if (opcode.length==3 && opcode.substring(0, 1)==1) {
+	  						validOp = true;
+	  					} else if (opcode.length==3 && opcode.substring(0, 1)==2) {
+	  						validOp = true;
+	  					} else if (opcode.length==3 && opcode.substring(0, 1)==7) {
+	  						validOp = true;
+	  					} else if (opcode.length==3 && opcode.substring(0, 1)==8) {
+	  						validOp = true;
+	  					} else if (opcode.length==3 && opcode.substring(0, 1)==6) {
+	  						validOp = true;
+	  					} 
+
+	  					//Final validation for a valid operator code.
+	  					if (validOp==false) {
+	  						console.log("LMC EO5: Invalid opcode at address "+dint+".");
+	  						process.exit(1);
+	  					} else {
+	  						mem[dint] = data[i][1]; 
+	  					}
+	  				}
+	  			}
+  			}
+
+  			//All memory saved, continuing to interpreter. PC starting at zero.
+  			interpreter(0);
 		});
 	}
 	catch (err) {
 		console.log("LMC E02: Failure loading LMC file.");
 		process.exit(1);
+	}
+}
+
+//Interpreter function, processes opcodes and provides function.
+//Takes a starting address.
+function interpreter(s) {
+	//Looping through memory, checking for start address.
+	for (i in mem) {
+		//Check for optype from start address onward.
+		if (i>=s) {
+			var opcode = mem[i].substring(0,1);
+
+			if (opcode==5) {
+				//load
+			} else if (opcode==3) {
+				//store
+			} else if (opcode==1) {
+				//add
+			} else if (opcode==2) {
+				//subtract
+			} else if (opcode==7) {
+				//branch if zero
+			} else if (opcode==8) {
+				//branch if zero or pos
+			} else if (opcode==6) {
+				//branch always
+			}
+		}
 	}
 }
